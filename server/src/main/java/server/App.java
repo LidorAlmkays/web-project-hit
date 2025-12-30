2a8c3b22fe8475c7aee1a1638bfb4fd983da9563
lidor <lidor1666689@gmail.com>
2025-12-30 22:51:40 +0200

    Add customer adding option and finding by תז and admin employee mock data to use for tests

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -4,9 +4,12 @@ import server.infustructre.InfrastructureFactory;
 import server.infustructre.adaptors.*;
 import server.api.SocketServer;
 import server.application.ApplicationFactory;
-import server.application.adaptors.*;
-import server.infustructre.InfrastructureFactory;
-import server.infustructre.adaptors.*;
+import server.application.adaptors.AuthService;
+import server.application.adaptors.BranchItemService;
+import server.application.adaptors.BranchService;
+import server.application.adaptors.EmployeeService;
+import server.application.adaptors.LoggerService;
+import server.application.adaptors.UserManagementService;
 
 public class App {
     private final InfrastructureFactory infrastructureFactory;
@@ -20,32 +23,30 @@ public class App {
     public void start() {
         System.out.println("Starting application");
 
-        System.out.println("Creating infrastructure");
-        CustomerRepository customerRepository = infrastructureFactory.createCustomerRepository();
-        BranchRepository branchRepository = infrastructureFactory.createBranchRepository();
-        BranchInventoryItemRepository branchInventoryItemRepository = infrastructureFactory
-                        .createBranchInventoryItemRepository();
-        EmployeeRepository employeeRepository = infrastructureFactory.createEmployeeRepository();
-        LogRepository logRepository = infrastructureFactory.createLogRepository();
-        System.out.println("Creating application");
-        UserManagementService userManagementService = applicationFactory
-                        .createUserManagementService(logRepository);
-        EmployeeService employeeService = applicationFactory.createEmployeeService(employeeRepository,
-                        branchRepository,
-                        logRepository);
-        BranchService branchService = applicationFactory.createBranchService(branchRepository,
-                        branchInventoryItemRepository, employeeRepository, logRepository);
-        LoggerService logService = applicationFactory.createLoggerService(logRepository);
-        AuthService authService = applicationFactory.createAuthService(employeeRepository, logRepository,
-                        userManagementService);
-        BranchItemService branchItemService = applicationFactory.createBranchItemService(branchRepository,
-                        branchInventoryItemRepository, customerRepository, logRepository);
-        CustomerService customerService = applicationFactory.createCustomerService(customerRepository,
-                logRepository);
-        System.out.println("Starting API");
-        SocketServer socketServer = new SocketServer(authService, logService, employeeService,
-                branchItemService, branchService, customerService);
-        socketServer.start();
+                System.out.println("Creating infrastructure");
+                CustomerRepository customerRepository = infrastructureFactory.createCustomerRepository();
+                BranchRepository branchRepository = infrastructureFactory.createBranchRepository();
+                BranchInventoryItemRepository branchInventoryItemRepository = infrastructureFactory
+                                .createBranchInventoryItemRepository();
+                EmployeeRepository employeeRepository = infrastructureFactory.createEmployeeRepository();
+                LogRepository logRepository = infrastructureFactory.createLogRepository();
+                System.out.println("Creating application");
+                UserManagementService userManagementService = applicationFactory
+                                .createUserManagementService(logRepository);
+                EmployeeService employeeService = applicationFactory.createEmployeeService(employeeRepository,
+                                branchRepository,
+                                logRepository);
+                BranchService branchService = applicationFactory.createBranchService(branchRepository,
+                                branchInventoryItemRepository, employeeRepository, logRepository);
+                LoggerService logService = applicationFactory.createLoggerService(logRepository);
+                AuthService authService = applicationFactory.createAuthService(employeeRepository, logRepository,
+                                userManagementService);
+                BranchItemService branchItemService = applicationFactory.createBranchItemService(branchRepository,
+                                branchInventoryItemRepository, customerRepository, logRepository);
+                System.out.println("Starting API");
+                SocketServer socketServer = new SocketServer(authService, logService, employeeService,
+                                branchItemService, branchService);
+                socketServer.start();
 
     }
 
