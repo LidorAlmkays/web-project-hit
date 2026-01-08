7a590e9a66d2585a02be8682faef796db1d4b1bc
lidor <lidor1666689@gmail.com>
2026-01-08 18:40:27 +0200

    Fix employee services to get emploee by email not uuid

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -2,14 +2,11 @@ package server;
 
 import server.infustructre.InfrastructureFactory;
 import server.infustructre.adaptors.*;
+import server.infustructre.adaptors.*;
 import server.api.SocketServer;
 import server.application.ApplicationFactory;
-import server.application.adaptors.AuthService;
-import server.application.adaptors.BranchItemService;
-import server.application.adaptors.BranchService;
-import server.application.adaptors.EmployeeService;
-import server.application.adaptors.LoggerService;
-import server.application.adaptors.UserManagementService;
+import server.application.adaptors.*;
+
 
 public class App {
     private final InfrastructureFactory infrastructureFactory;
