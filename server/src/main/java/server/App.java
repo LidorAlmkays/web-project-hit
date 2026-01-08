db5cb44fbfeb158dd9d11b97300ce7a0c3af27c1
mayeden12 <edeny8585@gmail.com>
2026-01-08 19:41:07 +0200

    1

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -1,60 +1,50 @@
 package server;
 
-import server.infustructre.InfrastructureFactory;
-import server.infustructre.adaptors.BranchInventoryItemRepository;
-import server.infustructre.adaptors.BranchRepository;
-import server.infustructre.adaptors.CustomerRepository;
-import server.infustructre.adaptors.EmployeeRepository;
-import server.infustructre.adaptors.LogRepository;
 import server.api.SocketServer;
 import server.application.ApplicationFactory;
-import server.application.adaptors.AuthService;
-import server.application.adaptors.BranchItemService;
-import server.application.adaptors.BranchService;
-import server.application.adaptors.CustomerService;
-import server.application.adaptors.EmployeeService;
-import server.application.adaptors.LoggerService;
-import server.application.adaptors.UserManagementService;
+import server.application.adaptors.*;
+import server.infustructre.InfrastructureFactory;
+import server.infustructre.adaptors.*;
 
 public class App {
-        private final InfrastructureFactory infrastructureFactory;
-        private final ApplicationFactory applicationFactory;
+    private final InfrastructureFactory infrastructureFactory;
+    private final ApplicationFactory applicationFactory;
 
-        public App() {
-                this.infrastructureFactory = new InfrastructureFactory();
-                this.applicationFactory = new ApplicationFactory();
-        }
+    public App() {
+        this.infrastructureFactory = new InfrastructureFactory();
+        this.applicationFactory = new ApplicationFactory();
+    }
 
-        public void start() {
-                System.out.println("Starting application");
+    public void start() {
+        System.out.println("Starting application");
 
-                System.out.println("Creating infrastructure");
-                CustomerRepository customerRepository = infrastructureFactory.createCustomerRepository();
-                BranchRepository branchRepository = infrastructureFactory.createBranchRepository();
-                BranchInventoryItemRepository branchInventoryItemRepository = infrastructureFactory
-                                .createBranchInventoryItemRepository();
-                EmployeeRepository employeeRepository = infrastructureFactory.createEmployeeRepository();
-                LogRepository logRepository = infrastructureFactory.createLogRepository();
-                System.out.println("Creating application");
-                UserManagementService userManagementService = applicationFactory
-                                .createUserManagementService(logRepository);
-                EmployeeService employeeService = applicationFactory.createEmployeeService(employeeRepository,
-                                branchRepository,
-                                logRepository);
-                BranchService branchService = applicationFactory.createBranchService(branchRepository,
-                                branchInventoryItemRepository, employeeRepository, logRepository);
-                LoggerService logService = applicationFactory.createLoggerService(logRepository);
-                AuthService authService = applicationFactory.createAuthService(employeeRepository, logRepository,
-                                userManagementService);
-                BranchItemService branchItemService = applicationFactory.createBranchItemService(branchRepository,
-                                branchInventoryItemRepository, customerRepository, logRepository);
-                CustomerService customerService = applicationFactory.createCustomerService(customerRepository,
-                                logRepository);
-                System.out.println("Starting API");
-                SocketServer socketServer = new SocketServer(authService, logService, employeeService,
-                                branchItemService, branchService, customerService);
-                socketServer.start();
+        System.out.println("Creating infrastructure");
+        CustomerRepository customerRepository = infrastructureFactory.createCustomerRepository();
+        BranchRepository branchRepository = infrastructureFactory.createBranchRepository();
+        BranchInventoryItemRepository branchInventoryItemRepository = infrastructureFactory
+                        .createBranchInventoryItemRepository();
+        EmployeeRepository employeeRepository = infrastructureFactory.createEmployeeRepository();
+        LogRepository logRepository = infrastructureFactory.createLogRepository();
+        System.out.println("Creating application");
+        UserManagementService userManagementService = applicationFactory
+                        .createUserManagementService(logRepository);
+        EmployeeService employeeService = applicationFactory.createEmployeeService(employeeRepository,
+                        branchRepository,
+                        logRepository);
+        BranchService branchService = applicationFactory.createBranchService(branchRepository,
+                        branchInventoryItemRepository, employeeRepository, logRepository);
+        LoggerService logService = applicationFactory.createLoggerService(logRepository);
+        AuthService authService = applicationFactory.createAuthService(employeeRepository, logRepository,
+                        userManagementService);
+        BranchItemService branchItemService = applicationFactory.createBranchItemService(branchRepository,
+                        branchInventoryItemRepository, customerRepository, logRepository);
+        CustomerService customerService = applicationFactory.createCustomerService(customerRepository,
+                logRepository);
+        System.out.println("Starting API");
+        SocketServer socketServer = new SocketServer(authService, logService, employeeService,
+                branchItemService, branchService, customerService);
+        socketServer.start();
 
-        }
+    }
 
 }
